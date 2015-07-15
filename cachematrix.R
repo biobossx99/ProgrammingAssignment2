547bc5c8473e4b9e055ed3e0b2774bf46fa12234
biobossx99 <kimchicat@gmail.com>
2015-07-14 21:09:21 -0700

    Assignment Completed

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Coursera R Programming - Allen Seol - Programming Assignment 2
 
-## Write a short comment describing this function
+
+## This Script creates a list of functions for a matrix that lets you store a matrix and cache it's inverse
+## setMatrix() - Sets the Value of the Matrix in the cache
+## getMatrix() - Gets the Value of the Matrix in the cache
+## setInvMat() - Sets the Inverse Matrix in the cache
+## getInvMat() - Gets the Inverse Matrix in the cache
 
 makeCacheMatrix <- function(x = matrix()) {
+  trix <- NULL ##trix is the cached matrix.  Initially set as NA with 1x1
+  
+  ##Sets argument y matrix to parent environment's x
+  setmat <- function(y) { 
+    x <<- y
+    trix <<- NULL
+  }
+  ##Returns the previously set X matrix
+  getmat <- function() x
+  
+  ##Caches the Inverse Arguement into the Trix
+  setinv <- function(inv) trix <<- inv
+    
+  ##Returns the Cached Inverse matrix
+  getinv <- function() trix
 
+  ## Returning a list of the four functions so that it can be subsetted
+ list(setmat = setmat, getmat = getmat, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
+## Return a matrix that is the inverse of 'x'
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  ##Let's retrieve the possible cached inv matrix
+  InvMatrix <- x$getinv() 
+  
+  ##If there is already an inverse Matrix in the list, it will return it
+  if(!is.null(InvMatrix)) {
+      message("Getting Cached Data!!!")
+      return(InvMatrix)
+  }
+  ##If there isn't a cached inv matrix
+  
+  temp <- x$getmax() ## gets original cached non-inv matrix
+  InvMatrix <- solve(temp,...)  ##inv original matrix 
+  
+  x$setinv(InvMatrix)  ##Caches newly made inverse matrix
+  
+  InvMatrix ##returns the newly made cached matrix  
+    
 }
+
+
+
